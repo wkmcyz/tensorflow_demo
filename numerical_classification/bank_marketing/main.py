d66a49f0421aca405d2d43c1d7475c551df9a8aa
wuchaochao <wuchaochao@yuanfudao.com>
2023-06-07 18:37:32 +0800

    MOD : a runnable

--- numerical_classification/bank_marketing/main.py
+++ numerical_classification/bank_marketing/main.py
@@ -1,31 +1,19 @@
+from typing import Tuple
+
 import numpy as np
 import tensorflow as tf
-from keras.feature_column.dense_features import DenseFeatures
+from keras.feature_column.dense_features_v2 import DenseFeatures
 from scipy.io import arff
-from tensorflow import feature_column
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from tensorflow import feature_column, Tensor
 from keras import layers
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
 # 加载数据
 data = arff.loadarff(open('dataset.arff', 'r'))
-dataframe = pd.DataFrame(data[0])  # 你的数据集的路径
-
-# Create a DataFrame with NaN and inf values
-# df = pd.DataFrame({'col1': [1.0, 2.0, np.nan, 4.0, 5.0], 'col2': [1.0, 2.0, 3.0, np.inf, 5.0]})
-df = dataframe
-# Check for NaN and inf values
-print(df.isna().any())  # Check for NaN values
-print(df.isin([np.inf, -np.inf]).any())  # Check for inf values
-
-# Replace NaN and inf values
-df = df.replace([np.inf, -np.inf], np.nan)  # Replace inf values with NaN
-df = df.fillna(0)  # Replace NaN values with 0
-
-# Convert to numpy array and make sure all data is float
-numpy_array = df.to_numpy().astype(np.float32)
-
-print("wahaha")
+dataframe = pd.DataFrame(data[0], dtype=np.float)  # 你的数据集的路径
+dataframe['Class'] = dataframe['Class'].astype(int)
 
 # 对于分类变量，我们需要将其转化为数值型变量。这可以通过one-hot编码或embedding等方式完成。
 # 在这个例子中，我们使用one-hot编码。
@@ -33,37 +21,59 @@ print("wahaha")
 # numeric_columns = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
 numeric_columns = ['V1', 'V6', 'V10', 'V12', 'V13', 'V14', 'V15']
 
-feature_columns = []
-# for header in categorical_columns:
-#     vocabulary = dataframe[header].unique()
-#     cat_col = feature_column.categorical_column_with_vocabulary_list(header, vocabulary)
-#     one_hot = feature_column.indicator_column(cat_col)
-#     feature_columns.append(one_hot)
+# 对类别标签进行编码
+label_encoder = LabelEncoder()
+dataframe['Class'] = label_encoder.fit_transform(dataframe['Class'])
 
+# 对特征值进行归一化
+# 对特征值进行归一化
+scaler = MinMaxScaler()
+dataframe[dataframe.columns[:-1]] = scaler.fit_transform(dataframe[dataframe.columns[:-1]])
+
+# 将数据集分割为训练集和测试集
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+
+
+def extract_x_y(df) -> Tuple:
+    # 选取 DataFrame 的前 9 列
+    df_selected = df.iloc[:, :7]
+
+    # 将 DataFrame 转换成 Tensor
+    x_tensor: Tensor = tf.convert_to_tensor(df_selected.values, dtype=tf.float32)
+
+    y = df.iloc[:, 7]
+    y_tensor = tf.convert_to_tensor(y.values, dtype=tf.float32)
+    return x_tensor, y_tensor
+
+
+train_x, train_y = extract_x_y(train)
+test_x, test_y = extract_x_y(test)
+
+feature_columns = []
 for header in numeric_columns:
     numeric_col = feature_column.numeric_column(header)
     feature_columns.append(numeric_col)
-
 # 现在我们已经定义了我们的特征列，我们可以构建我们的模型。
 model = tf.keras.Sequential([
-    DenseFeatures(feature_columns),
+    # DenseFeatures(feature_columns),
+    layers.Dense(128, input_dim=train_x.shape[1], activation='relu'),
+    # layers.Dense(128, activation='relu', input_shape=(7,)),
     layers.Dense(128, activation='relu'),
     layers.Dense(128, activation='relu'),
+    # layers.Dense(2, activation='softmax')
     layers.Dense(1, activation='sigmoid')
 ])
 
 # 我们将使用二元交叉熵作为损失函数，优化器使用Adam。
 model.compile(optimizer='adam',
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              # loss=tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=['accuracy'])
-
-# 将数据集分割为训练集和测试集
-train, test = train_test_split(dataframe, test_size=0.2)
-train, val = train_test_split(train, test_size=0.2)
-
 # 训练模型
-model.fit(train, epochs=10)
+print("开始 fit ")
+model.fit(train_x, train_y, validation_split=0.2, epochs=100, batch_size=10)
 
 # 验证模型
-loss, accuracy = model.evaluate(test)
+loss, accuracy = model.evaluate(test_x, test_y)
 print(f"loss : {loss}", "Accuracy", accuracy)
